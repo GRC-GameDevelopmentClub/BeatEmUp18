f3262a8d8b750f545b0378c44caf8c88eee4673d
Miguel Garnica <mgarnica123@aol.com>
2018-07-03 22:56:00 -0700

    Player Movement
    Added:
        - PlayerMovement Script
        - Player and Platform prefabs
        - Player Ground, layers and tags
        - Prefabs folder
    Updated:
        - Gitignore ignore .lock files

---
+++ BeatEmUp2018/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed, jumpForce;
+    public KeyCode left, right, jump;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
+ 
+        if (Input.GetKey(left))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }else if (Input.GetKey(right))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if(grounded && Input.GetKey(jump))
+        {
+            rb.AddForce(new Vector2(0,jumpForce));
+        }
+ 
+		
+	}
+}
