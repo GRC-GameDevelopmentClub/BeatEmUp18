3427995a98cb191a97f13aed2c188e0691ef54ad
Miguel Garnica <mgarnica123@aol.com>
2018-07-08 15:02:02 -0700

    Attack Scripts
    Added:
        - Attack scripts ( allows for attacks on entites with either Health.cs or RigidBody2d components)
        - Entity prefab for testing purposes
    Updated:
        - Player Prefab

---
+++ BeatEmUp2018/Assets/Scripts/Attack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public float damage;
+    public float knockBackForce;
+    public float attackTime;
+    public KeyCode attack;
+    private BoxCollider2D attackBox;
+    Vector2 direction;
+    // Use this for initialization
+    void Start () {
+        attackBox = GetComponent<BoxCollider2D>();
+	}
+
+    float timer = 0;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(attack))
+        {
+            attackBox.enabled = true;
+        }
+
+        if (attackBox.enabled)
+        {
+            timer += Time.deltaTime;
+            if (timer > attackTime)
+            {
+                attackBox.enabled = false;
+                timer = 0;
+            }
+        }
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Health hp = collision.GetComponent<Health>();
+        if (hp == null)
+            return;
+        else
+        {
+            hp.TakeDamage(damage);
+        }
+
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        if (rb == null)
+            return;
+        else
+        {
+            direction = (collision.transform.position - this.transform.position).normalized;
+            rb.AddForce(new Vector2(direction.x * knockBackForce, direction.y * knockBackForce * 2));
+        }
+   
+    }
+}
