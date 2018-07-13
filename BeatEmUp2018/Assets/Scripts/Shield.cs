7aced3ae1a5e531644ed1cae8ac96f02c0cd4b96
Miguel Garnica <mgarnica123@aol.com>
2018-07-12 19:03:44 -0700

    Player Shield Ability
    Added:
        - Shield.cs Script (Creates a 'shield' around player entity)
    Updated:
         - Player Prefab (now includes shield)
        - gitIgnore ignore vs studio files and extra unity files

---
+++ BeatEmUp2018/Assets/Scripts/Shield.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shield : MonoBehaviour {
+
+    public float shieldTime;
+    public float coolDown;
+    public KeyCode shield;
+    private CircleCollider2D circleCol;
+    private SpriteRenderer sr;
+    private bool isShield;
+
+	// Use this for initialization
+	void Start () {
+        circleCol = GetComponent<CircleCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        circleCol.enabled = false;
+        sr.enabled = false;
+	}
+
+    float sTimer = 0;
+    float cTimer = 0;
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(shield) && cTimer == 0)
+        {
+            isShield = true;
+        }
+
+        if (isShield)
+        {
+            circleCol.enabled = true;
+            sr.enabled = true;
+
+            sTimer += Time.deltaTime;
+            if (sTimer > shieldTime)
+            {
+                isShield = false;
+                sTimer = 0;
+                cTimer = coolDown;
+            }
+        }
+        else
+        {
+            circleCol.enabled = false;
+            sr.enabled = false;
+            cTimer -= Time.deltaTime;
+            if(cTimer < 0)
+            {
+                cTimer = 0;
+            }
+        }
+
+	}
+
+}
